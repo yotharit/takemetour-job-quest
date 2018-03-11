da0d344091852eda9bdeb53c6f38b1095c044f50
Tharit Pongsaneh <tharit.p@ku.th>
2018-03-12 01:01:48 +0700

    Fix First Version

--- chucknorris/src/components/Header.js
+++ chucknorris/src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CnLogoWithName from './asset/CN-logo-with-name.svg'
-import { stack as Menu } from 'react-burger-menu'
 
 
 class Header extends Component {
@@ -17,20 +16,8 @@ class Header extends Component {
 
     return (
       <div className="container-fluid" style={barStyle}>
-        <div class="d-flex flex-row justify-content-between my-flex-container">
-          <div class="p-2 my-flex-item">
-            <Menu style>
-              <a id="home" className="menu-item" href="/">Home</a>
-              <a id="about" className="menu-item" href="/about">About</a>
-              <a id="contact" className="menu-item" href="/contact">Contact</a>
-              <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-            </Menu>
-          </div>
-          <div class="p-2 my-flex-item">
+        <div class="d-flex flex-row justify-content-center my-flex-container">
             <img src={ CnLogoWithName } height="80"  width="80" alt=""/>
-          </div>
-          <div class="p-2 my-flex-item">
-          </div>
         </div>
       </div>
     );
